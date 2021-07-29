c66fd9b078aecc2a440497be4db8b4fad7368ac6
jadahbakar <dedy.styawan@gmail.com>
2021-07-29 10:10:44 +0700

    handler_test

--- internal/domain/master/bod/handler_test.go
+++ internal/domain/master/bod/handler_test.go
@@ -2,33 +2,85 @@ package bod_test
 
 import (
 	"log"
-	"net/http"
-	"net/http/httptest"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 
-	"github.com/bxcodec/faker"
-	"github.com/gofiber/fiber/v2"
 	"github.com/jadahbakar/asastarealty-backend/app"
 	"github.com/jadahbakar/asastarealty-backend/internal/config"
-	"github.com/jadahbakar/asastarealty-backend/internal/domain/master/bod"
-	"github.com/jadahbakar/asastarealty-backend/mocks"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestGetAll(t *testing.T) {
-	var mockBod bod.Bod
-	err := faker.FakeData(&mockBod)
-	assert.NoError(t, err)
-	mockService := new(mocks.BodService)
-	mockListBod := make([]bod.Bod, 0)
-	mockListBod = append(mockListBod, mockBod)
-	mockService.On("Fetch").Return(mockListBod, nil)
+// func TestGetAll(t *testing.T) {
+// 	// var mockBod bod.Bod
+// 	// err := faker.FakeData(&mockBod)
+// 	// assert.NoError(t, err)
+// 	// mockService := new(mocks.BodService)
+// 	// mockListBod := make([]bod.Bod, 0)
+// 	// mockListBod = append(mockListBod, mockBod)
+// 	// mockService.On("Fetch").Return(mockListBod, nil)
 
-	// f := fiber.New()
+// 	// _, b, _, _ := runtime.Caller(0)
+// 	// root := filepath.Join(filepath.Dir(b), "../../../../")
+// 	// // Setup Config
+// 	// config, err := config.New(root)
+// 	// if err != nil {
+// 	// 	log.Printf("error Config New -> %v\n", err)
+// 	// }
+// 	// assert.NotNil(t, config)
+// 	// apps := app.New(config, nil)
+// 	// assert.NotNil(t, apps)
+// 	// engine := apps.GetEngine()
+// 	// assert.NotNil(t, engine)
+// 	// logger := apps.GetLogger()
+// 	// assert.NotNil(t, logger)
+
+// 	// // req, err := http.NewRequest(fiber.MethodGet, "/api/v1/bod", strings.NewReader(""))
+// 	// req, err := http.NewRequest(fiber.MethodGet, "/api/v1/bod", nil)
+// 	// assert.NoError(t, err)
+
+// 	// rec := httptest.NewRecorder()
+
+// 	// // res, err := engine.Test(req, -1)
+
+// 	// // c := fiber.Ctx(req, rec)
+// 	// // handler := bod.BodHandler{bod.BodService: mockService}
+
+// 	// // err = handler.GetAll(ctx)
+// 	// require.NoError(t, err)
+
+// 	// Define Fiber config.
+// 	config := fiber.Config{}
+
+// 	// Define a new Fiber app with config.
+// 	app := fiber.New(config)
+// 	// Create route with GET method for test:
+// 	app.Get("/api/v1", func(c *fiber.Ctx) error {
+// 		return c.SendString("hello, customVerb!")
+// 	})
+// 	// http.Request
+// 	req := httptest.NewRequest("GET", "/api/v1", nil)
+
+// 	// http.Response
+// 	resp, err := app.Test(req)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	// Do something with results:
+// 	if resp.StatusCode == 200 {
+// 		body, _ := ioutil.ReadAll(resp.Body)
+// 		fmt.Println(string(body)) // => hello, customVerb!
+// 	}
+// 	if resp.StatusCode != 200 {
+// 		t.Error(resp.StatusCode)
+// 	}
+
+// }
+
+func TestGetAll(t *testing.T) {
+	// Define Fiber config.
+	// config := fiber.Config{}
 	_, b, _, _ := runtime.Caller(0)
 	root := filepath.Join(filepath.Dir(b), "../../../../")
 	// Setup Config
@@ -41,17 +93,5 @@ func TestGetAll(t *testing.T) {
 	assert.NotNil(t, apps)
 	engine := apps.GetEngine()
 	assert.NotNil(t, engine)
-	logger := apps.GetLogger()
-	assert.NotNil(t, logger)
-
-	req, err := http.NewRequest(fiber.MethodGet, "/api/v1/bod", strings.NewReader(""))
-	assert.NoError(t, err)
-
-	rec := httptest.NewRecorder()
-	res, err := engine.Test(req, -1)
-	// c := fiber.Ctx(req, rec)
-	handler := bod.BodHandler{bod.BodService: mockService}
-	// err = handler.GetAll(ctx)
-	require.NoError(t, err)
 
 }
